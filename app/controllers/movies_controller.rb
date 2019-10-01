dfed5b83a2d2663ecb84da0396607c3a4fcfe082
Daniel P <dvp416@gmail.com>
2019-09-30 19:19:19 -0500

    hilite test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,6 +12,12 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+    if params[:sort_by] == 'title'
+      @title_header = 'hilite'
+    elsif params[:sort_by] == 'release_date'
+      @release_header = 'hilite'
+    end
+
   end
 
   def new
