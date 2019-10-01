9fe3366982aa4a6c64cda676c72f21c28ff9df57
Daniel P <dvp416@gmail.com>
2019-10-01 16:03:04 -0500

    Boxes stay ticked test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,7 +16,7 @@ class MoviesController < ApplicationController
 
     @checked_boxes = check_boxes
     @checked_boxes.each do |rating|
-      params[:rating] = true
+      params[rating] = true
     end
 
 
