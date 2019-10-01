ef6206d0e25b34cd59617c40b4f14bdbfa78f3d6
Daniel P <dvp416@gmail.com>
2019-10-01 12:11:57 -0500

    test all_ratings

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,16 +14,16 @@ class MoviesController < ApplicationController
 
     #@all_ratings = ['G', 'PG', 'R']
     @all_ratings = Movie.order(:rating).select(:rating).map(&:rating).uniq
-    #@checks = checked_boxes
-    #@checks.each do |rating|
-    #  params[rating] = true
-    #end
+    @checks = checked_boxes
+    @checks.each do |rating|
+      params[rating] = true
+    end
 
     if params[:sort]
       @movies = Movie.order(params[:sort])
     else
       #@movies = Movie.where(:rating => @checks)
-      @movies = Movie.where(:rating => @all_ratings)
+      @movies = Movie.where(:rating => @checks)
     end
 
 
