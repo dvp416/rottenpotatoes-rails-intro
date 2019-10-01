f47f4d047bab0af015527e4bcce21de76633fd8b
Daniel P <dvp416@gmail.com>
2019-10-01 11:40:31 -0500

    test checkedboxes revert

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,7 +12,7 @@ class MoviesController < ApplicationController
 
   def index
 
-    @all_ratings = ['G', 'PG', 'PG-13', 'R']
+    #@all_ratings = ['G', 'PG', 'PG-13', 'R']
 
     if params[:sort]
       @movies = Movie.order(params[:sort])
