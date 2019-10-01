c967aabe9f8ecde0e3874ec98219b39c1bcc24d1
Daniel P <dvp416@gmail.com>
2019-10-01 16:07:32 -0500

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,20 +13,18 @@ class MoviesController < ApplicationController
   def index
 
     @all_ratings = Movie.pluck(:rating).uniq
-    @checked_boxes = nil
+
+    @checked_boxes = check_boxes
+    @checked_boxes.each do |rating|
+      params[rating] = true
+    end
+
 
     #Session handling for ratings filter
    if params[:ratings]
      session[:ratings] = params[:ratings]
-     @checked_boxes = params[:ratings].keys
-   else
-     @checked_boxes = @all_ratings
    end
    session[:ratings] = session[:ratings] || Hash[ @all_ratings.map {|ratings| [ratings, 1]} ]
-
-   @checked_boxes.each do |rating|
-     params[rating] = true
-   end
 
    #Session handling for title/date sort
    if params[:sort]
